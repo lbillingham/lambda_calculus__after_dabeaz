4ca3a0ed026f82858017edb8da099b8aad877b9f
Laurence Billingham <laurence@travelnest.com>
2019-05-17 23:37:19 +0100

    :memo: document some equivilences

--- boolean.py
+++ boolean.py
@@ -1,8 +1,16 @@
 def TRUE(x):
+    """same as
+        - `TRUE = lambda x: lambda y: x`
+        - `switch.LEFT`
+    """
     return lambda y: x
 
 
 def FALSE(x):
+    """same as
+        - `FALSE = lambda x: lambda y: y`
+        - `switch.RIGHT`
+    """
     return lambda y: y
 
 
@@ -11,6 +19,9 @@ def NOT(x):
 
 
 def AND(x):
+    """same as
+        - return lambda y: x(y)(x)
+    """
     # could also x(y)(x)
     return lambda y: x(y)(FALSE)
 
